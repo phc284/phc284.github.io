466a51a7a36addaceb35cd61cd13f5a89022c696
Peter Choi <phc284@gmail.com>
2018-05-10 15:05:59 -0500

    rendering projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,53 +1,24 @@
 import React, { Component } from 'react';
 
+import { projects } from '../helpers/data';
+
+const ProjectList = projects.map((project, index) => {
+  return (
+    <div class="project-wrap" key={index}>
+      <a target="_blank" rel="noopener noreferrer" href={project.link}>
+        <h3 class="project-desc">{project.name}</h3>
+      </a>
+      <p>{project.description}</p>
+    </div>
+  );
+});
+
 export class Projects extends Component {
   render() {
     return (
       <div class="container-fluid text-center projects" id="projects">
         <h2 class="projects-title">My Projects</h2>
-        <div class="projects-grid">
-          <div class="project-wrap">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://cowboys-resource.herokuapp.com"
-            >
-              <img class="project-item" src="Images/cowboys-resource.png" />><h3 class="project-desc">
-                Dallas Cowboys Resource
-              </h3>
-            </a>
-          </div>
-          <div class="project-wrap">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/phc284/newnews"
-            >
-              <img class="project-item" src="Images/newnews.png" />
-              <h3 class="project-desc">NewNews</h3>
-            </a>
-          </div>
-          <div class="project-wrap">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="http://scream-machine.herokuapp.com"
-            >
-              <img class="project-item" src="Images/screamMachine.png" />
-              <h3 class="project-desc">Scream Machine</h3>
-            </a>
-          </div>
-          <div class="project-wrap">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="http://getmea.gift"
-            >
-              <img class="project-item" src="Images/getmeagift.png" />
-              <h3 class="project-desc">GetMeAGift</h3>
-            </a>
-          </div>
-        </div>
+        <div class="projects-grid">{ProjectList}</div>
       </div>
     );
   }
